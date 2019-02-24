6e144cd63d75c7726a2b1c89a09163668335d8e4
keclements <kristin.clements@gmail.com>
2019-02-24 17:59:22 -0500

    difficulty level assigned

--- word_game.py
+++ word_game.py
@@ -6,33 +6,48 @@ with open('words.txt') as file:
     word_list = file.read().splitlines() 
    #  print(word_list) #valid test of print word_list
 
-text = text
+easy_list = []
+medium_list = []
+hard_list = []
 
-# def random
+#function defile difficulty
+def easy_mode():
+    with open('words.txt') as file: #file variable defined as words.txt
+        for small_words in file:
+            if len(small_words) >= 4 and len(small_words) <= 6:
+                easy_list.append(small_words)
+
+def medium_mode():
+    with open('words.txt') as file: #file variable defined as words.txt
+        for medium_words in file:
+            if len(medium_words)  >= 6 and len(medium_words) <=8:
+                medium_list.append(medium_words)
+
+def hard_mode():
+    with open('words.txt') as file:
+        for hard_words in file:
+            if len(hard_words) >= 8:
+                hard_list.append(hard_words)
+
+def difficulty_level():
+    chosen_difficulty = input( "Choose difficulty of the game? Levels are easy, medium, hard word. ")
+    if chosen_difficulty == 'easy':
+        easy_mode()
+    elif chosen_difficulty == 'medium':
+        medium_mode()
+    else hard_mode =='hard':
+        hard_mode()
+    
 
 
-# # open file get words
 
 
-# # # Letter in word FUNCTION code thoughts
-# def letter_in_word (letter, random_generated_word):
-#     letter = input("Guess a letter for this word?") # renamed from user input and named in letter variable
-#     used_letters.append(letter)
-#     if letter in random_generated_word:
-#         return letter #return closes if statement above
-#     else:
-#         return "_"
+make random word
+from word in word_list
 
-# def letter_not_in_word(letter, random_generated_word):
-#     letter = input("Guess a letter for this word?")
-#     used_letters.append(letter)
-#     if letter not in random_generated_word:
-#         allowed_guess -= 1
-#         print("You have " + allowed_guess + " left")
-#         return letter
-#     else:
-#         return
 
+assign difficulty to word
+    if random_word = len(word_list) < 4
 
 # # nameoflist.apppend(letter)
 
