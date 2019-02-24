962bad4db9ed57f009dc9cf48e21da970a901375
keclements <kristin.clements@gmail.com>
2019-02-24 16:57:37 -0500

    open words changed

--- word_game.py
+++ word_game.py
@@ -3,37 +3,40 @@ import string
 import random
 
 with open('words.txt') as file: 
-    word_list = file.readlines().splitline()
-    # print(word_list) #valid test of print word_list
-def random
+    word_list = file.read().splitlines() 
+   #  print(word_list) #valid test of print word_list
+
+text = text
+
+# def random
 
 
-# open file get words
+# # open file get words
 
 
-# # Letter in word FUNCTION code thoughts
-def letter_in_word (letter, random_generated_word):
-    letter = input("Guess a letter for this word?") # renamed from user input and named in letter variable
-    used_letters.append(letter)
-    if letter in random_generated_word:
-        return letter #return closes if statement above
-    else:
-        return "_"
+# # # Letter in word FUNCTION code thoughts
+# def letter_in_word (letter, random_generated_word):
+#     letter = input("Guess a letter for this word?") # renamed from user input and named in letter variable
+#     used_letters.append(letter)
+#     if letter in random_generated_word:
+#         return letter #return closes if statement above
+#     else:
+#         return "_"
 
-def letter_not_in_word(letter, random_generated_word):
-    letter = input("Guess a letter for this word?")
-    used_letters.append(letter)
-    if letter not in random_generated_word:
-        allowed_guess -= 1
-        print("You have " + allowed_guess + " left")
-        return letter
-    else:
-        return
-
-
-# nameoflist.apppend(letter)
+# def letter_not_in_word(letter, random_generated_word):
+#     letter = input("Guess a letter for this word?")
+#     used_letters.append(letter)
+#     if letter not in random_generated_word:
+#         allowed_guess -= 1
+#         print("You have " + allowed_guess + " left")
+#         return letter
+#     else:
+#         return
 
 
+# # nameoflist.apppend(letter)
 
-####
-line 8 test to print randonm word still has \n
+
+
+# ####
+# line 8 test to print randonm word still has \n
