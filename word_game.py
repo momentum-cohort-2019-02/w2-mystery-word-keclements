13f68d6da764fd9eca6f71e588e5c0aece058019
keclements <kristin.clements@gmail.com>
2019-02-24 16:38:41 -0500

    print words from list validated

--- word_game.py
+++ word_game.py
@@ -1,27 +1,38 @@
-with open("words.txt") as word_file:
+with open("words.txt") as word_file: 
     word_list = word_file.readlines()
     print(word_list)
 
 import random
+import string
 
-# Letter in word FUNCTION code thoughts
-def letter_in_word (letter, random_generated_word):
-    letter = input("Guess a letter for this word?") # renamed from user input and named in letter variable
-    used_letters.append(letter)
-    if letter in random_generated_word:
-        return letter #return closes if statement above
-    else:
-        return "_"
+                    # print("word_list") # prints words valid test
 
-def letter_not_in_word(letter, random_generated_word):
-    letter = input("Guess a letter for this word?")
-    used_letters.append(letter)
-    if letter not in random_generated_word:
-        allowed_guess -= 1
-        print("You have " + allowed_guess + " left")
-        return letter
-    else:
-        return
+# open file get words
 
 
-# nameoflist.apppend(letter)
+# # Letter in word FUNCTION code thoughts
+# def letter_in_word (letter, random_generated_word):
+#     letter = input("Guess a letter for this word?") # renamed from user input and named in letter variable
+#     used_letters.append(letter)
+#     if letter in random_generated_word:
+#         return letter #return closes if statement above
+#     else:
+#         return "_"
+
+# def letter_not_in_word(letter, random_generated_word):
+#     letter = input("Guess a letter for this word?")
+#     used_letters.append(letter)
+#     if letter not in random_generated_word:
+#         allowed_guess -= 1
+#         print("You have " + allowed_guess + " left")
+#         return letter
+#     else:
+#         return
+
+
+# # nameoflist.apppend(letter)
+
+
+
+#####
+# line 8 test to print randonm word still has \n
