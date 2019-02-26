09e551f10a4ccb4b35986249d2ae1d9a729894e4
KE Clements <31896853+keclements@users.noreply.github.com>
2019-02-26 08:08:02 -0500

    pseudo code for 2 choice game

---
+++ mystery_word.py
@@ -0,0 +1,14 @@
+import random
+print("hi.")
+
+# ask 1 or 2
+pick = input("pick 1 or 2")
+
+# get input of 1 or 2
+# if 1 
+	# print ("1 is wrong")
+# else print ("2 is right")
+# print ("Good bye ")
+#game stops
+
+if
